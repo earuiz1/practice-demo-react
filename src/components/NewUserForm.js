d41a750e1ab2c62b8e45178574bb6b02b9eaa68e
earuiz1 <earuiz1196@gmail.com>
2022-08-12 03:24:02 -0500

    Clear the inputs after form submission using two-way binding.

--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,50 +1,58 @@
-import React, {useState} from "react";
-import './NewUserForm.css';
+import React, { useState } from "react";
+import "./NewUserForm.css";
+
 
 function NewUserForm(props) {
+  
+  //Setting states for username and age
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
 
-const [username, setUsername] = useState('');
-const [age, setAge] = useState('');
 
-const usernameHandler = (event) => {
+  const usernameHandler = (event) => {
     //console.log(event.target.value);
     setUsername(event.target.value);
-}
+  };
 
-const ageHandler = (event) => {
+  const ageHandler = (event) => {
     //console.log(event.target.value);
     setAge(event.target.value);
-}
+  };
 
-const submitHandler = (event) => {
-
+ 
+  const submitHandler = (event) => {
     event.preventDefault();
 
-     const user = {
-        userName : username,
-        userAge : age,
-     }
-     
-     //console.log('In NewUser.js', user);
+    const user = {
+      userName: username,
+      userAge: age,
+    };
 
-     props.onSubmitForm(user);
+    //console.log('In NewUser.js', user);
+    props.onSubmitForm(user);
 
-}
+    //Clear the inputs using two-way binding
+    setUsername("");
+    setAge("");
+  };
+
   return (
     <div>
       <form onSubmit={submitHandler}>
         <div className="form-controls">
           <div className="form-control">
             <label>Username</label>
-            <input type="text" onChange={usernameHandler} />
+            <input type="text" onChange={usernameHandler} value={username} />
           </div>
           <div className="form-control">
             <label>Age(Years)</label>
-            <input type="number" onChange={ageHandler}/>
+            <input type="number" onChange={ageHandler} value={age} />
           </div>
         </div>
         <div className="form-buttons">
-          <button type="submit">Add User</button>
+          <button type="submit">
+            Add User
+          </button>
         </div>
       </form>
     </div>
